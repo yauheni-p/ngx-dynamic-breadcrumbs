695ee53ce4a54a6363401ff11acab5dbba61bc24
yauheni.putko <yauheni.putko@itexus.com>
2021-03-13 12:38:33 +0300

    initial commit

---
+++ apps/example/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'breadcrumbs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'example';
+}
